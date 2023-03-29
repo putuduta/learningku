a5a3136d45b6a25b1ef1546cc35f8d7b1c9391b1
putuduta <putu.duta@gmail.com>
2023-03-29 20:23:09 +0700

    Update Students Page

--- resources/views/class/student-list.blade.php
+++ resources/views/class/student-list.blade.php
@@ -1,26 +1,32 @@
 <x-app title="Students - Learningku">
-     <x-slot name="navbar"></x-slot>
- 
-     <section id="headerClassSubject">
-        <div id="content" class="container pt-5 mt-5">
-            <div class="mb-3">
-                <span class="fa-stack fa-md ms-n1">
-                    <i class="fas fa-circle fa-stack-2x text-orange"></i>
-                    <a href="{{ url()->previous() }}" class="fas fa-arrow-left fa-stack-1x fa-inverse text-light" style="text-decoration: none;"></a>
-                </span>
-            </div>
+    <style>
+        .fa-stack.small { font-size: 0.5em; }
+        i { vertical-align: middle; }
+    </style>
+    <x-slot name="navbar"></x-slot>
 
-            <div class="card shadow-sm border-0 mb-3">
-                <div class="card-body">
-                    <div class="row align-items-center">
-                        <div class="col-md-10">
-                            <h1 class="fw-bold">Mata Pelajaran {{ $classSubject->name }}</h1>
-                            <h3>Kelas {{ $classSubject->className }} - {{ $classSubject->schoolYear }} {{ $classSubject->semester }}</h3>
-                            <h5>Guru Pengajar {{ $classSubject->teacherName }} </h5>
-                        </div>           
+    <section id="headerClassSubject">
+        <div id="content" class="container pt-5 mt-5">
+            <section id="headerClassDetail">
+                <div class="mb-3">
+                    <span class="fa-stack fa-md ms-n1">
+                        <i class="fas fa-circle fa-stack-2x text-orange"></i>
+                        <a href="{{ url()->previous() }}" class="fas fa-arrow-left fa-stack-1x fa-inverse text-light" style="text-decoration: none;"></a>
+                    </span>
+                </div>
+    
+                <div class="card shadow-sm border-0 mb-3">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-md-10">
+                                <h2 class="fw-bold">Mata Pelajaran {{ $classSubject->name }}</h2>
+                                <h5 class="pb-2">Guru Pengajar: {{ $classSubject->teacherName }} - {{ $classSubject->teacherNuptk }}</h5>
+                                <h5><span class="fa-stack small pb-4"><i class="fas fa-circle fa-stack-2x text-orange"></i><i class="fas fa-home fa-stack-1x fa-2xs fa-inverse text-white"></i></span> {{ $classSubject->className }} - {{ $classSubject->schoolYear }} {{ $classSubject->semester }} <span class="fa-stack small pb-4"><i class="fas fa-circle fa-stack-2x text-orange"></i><i class="fas fa-user fa-stack-1x fa-2xs fa-inverse text-white"></i></span> Wali Kelas: {{ $classSubject->homeRoomTeacherName }} - {{ $classSubject->homeRoomTeacherNuptk }}</h5>
+                            </div>           
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
     
             <nav class="" style="font-size:1.25rem">
                 {{-- <div class="container"> --}}
@@ -51,14 +57,14 @@
           <div class="table-responsive pt-2">
                <table class="table table-hover table-bordered">
                     <thead class="table-dark">
-                         <th class="align-middle text-center">ID</th>
+                         <th class="align-middle text-center">No</th>
                          <th class="align-middle text-center">NISN</th>
                          <th class="align-middle text-center">Student Name</th>
                     </thead>
                     <tbody>
-                         @foreach ($students as $student)
+                         @foreach ($students as $index=>$student)
                               <tr>
-                                   <td class="align-middle text-center">{{$student->id}}</td>
+                                   <td class="align-middle text-center">{{$index+1}}</td>
                                    <td class="align-middle text-center">{{$student->nisn}}</td>
                                    <td class="align-middle text-center">{{$student->name}}</td>
                               </tr>
