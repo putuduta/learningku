c1de2c411a2e044952252462f44589b7a8ea79f6
philipuswa77 <philipus.wijaya@gmail.com>
2023-04-05 19:28:39 +0700

    add admin seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,8 +17,8 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'name' => 'Felix',
-                'email' => 'felix@gmail.com',
+                'name' => 'Agus',
+                'email' => 'agus@gmail.com',
                 'gender' => 'Male',
                 'role_id' => '1',
                 'password' => Hash::make('kualat')
