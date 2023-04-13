b85b2d17d2038b0b56f4b325225b5b766fa7d2c8
philipuswa77 <philipus.wijaya@gmail.com>
2023-04-13 17:41:22 +0700

    add attendance validation checkbox

--- resources/views/attendance/create.blade.php
+++ resources/views/attendance/create.blade.php
@@ -23,7 +23,7 @@
                     </div>
                     <hr>
                     <div class="my-3">
-                        <input type="checkbox" onClick="presentAll(this)" class="mb-3"> Present all
+                        <input type="checkbox" onClick="presentAll(this)" class="presentAll mb-3"> Present all
                         <div class="table-responsive">
                             <table class="table table-hover table-bordered">
                                 <thead class="table-dark">
@@ -42,19 +42,19 @@
                                         <td class="align-middle text-center">{{ $student->studentNisn }}</td>
                                         <td class="align-middle text-center">{{ $student->studentName }}</td>
                                         <td class="align-middle text-center">
-                                            <input type="checkbox" value="1" class="form-check-input attend"
+                                            <input type="checkbox" value="1" class="form-check-input attend radio"
                                                 name="present-{{$student->studentId}}">
                                         </td>
                                         <td class="align-middle text-center">
-                                            <input type="checkbox" value="1" class="form-check-input"
+                                            <input type="checkbox" value="1" class="form-check-input radio"
                                                 name="sick-{{$student->studentId}}">
                                         </td>
                                         <td class="align-middle text-center">
-                                            <input type="checkbox" value="1" class="form-check-input"
+                                            <input type="checkbox" value="1" class="form-check-input radio"
                                                 name="absencePermit-{{$student->studentId}}">
                                         </td>
                                         <td class="align-middle text-center">
-                                            <input type="checkbox" value="1" class="form-check-input"
+                                            <input type="checkbox" value="1" class="form-check-input radio"
                                                 name="absent-{{$student->studentId}}">
                                         </td>
                                     </tr>
@@ -73,10 +73,40 @@
 
 <script>
     function presentAll(source) {
+        //uncheck all checkbox
+        allCheckbox = document.getElementsByClassName('form-check-input');
+        for (var i = 0, n = allCheckbox.length; i < n; i++) {
+            allCheckbox[i].checked = source.unchecked;
+        }
+
         checkboxes = document.getElementsByClassName('attend');
         for (var i = 0, n = checkboxes.length; i < n; i++) {
             checkboxes[i].checked = source.checked;
         }
     }
 
+    $("input:checkbox").on('click', function() {
+        var box = $(this);
+        if (box.is(":checked")) {
+            if(box.attr('class') != 'presentAll mb-3'){
+                var studentId = box.attr("name").split('-')[1];
+            }
+
+            var groupPresent = "input:checkbox[name=present-" + studentId + "]";
+            var groupSick = "input:checkbox[name=sick-" + studentId + "]";
+            var groupAbsencePermit = "input:checkbox[name=absencePermit-" + studentId + "]";
+            var groupAbsent = "input:checkbox[name=absent-" + studentId + "]";
+            
+            $(groupPresent).prop("checked", false);
+            $(groupSick).prop("checked", false);
+            $(groupAbsencePermit).prop("checked", false);
+            $(groupAbsent).prop("checked", false);
+            box.prop("checked", true);
+        }else{
+            box.prop("checked", false);
+        }
+    });
 </script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="script.js"></script>
