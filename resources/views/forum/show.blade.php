a5ed8c89084a232eb69acfd2484fb403b2813901
putuduta <putu.duta@gmail.com>
2023-04-13 10:43:47 +0700

    Rename Edit Forum

--- resources/views/forum/show.blade.php
+++ resources/views/forum/show.blade.php
@@ -62,7 +62,7 @@
                     <div class="modal-dialog modal-dialog-centered modal-lg">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Edit Forum</h5>
+                                <h5 class="modal-title" id="exampleModalLabel">Edit Forum Thread</h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
                             </div>
