d7ad9ec8df29eedf28dd3d8b02b24d2236cdae46
philipuswa77 <philipus.wijaya@gmail.com>
2023-05-23 16:33:24 +0700

    adjust material controller

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -14,10 +14,10 @@ class MaterialController extends Controller
     public function index($classSubjectId){
         return view('material.index', [
             'classSubject' => ClassSubject::select('class_subjects.id as id', 'class_subjects.name as name',
-                'class_headers.name as className', 'school_years.year as schoolYear', 'school_years.semester as semester', 'users.name as teacherName',
+                'class_headers.name as className', 'school_year.year as schoolYear', 'school_year.semester as semester', 'users.name as teacherName',
                 'userB.name as homeRoomTeacherName', 'userB.user_code as homeRoomTeacherNuptk', 'users.user_code as teacherNuptk')
                 ->join('class_headers', 'class_headers.id', 'class_subjects.class_header_id')
-                ->join('school_years', 'school_years.id', 'class_headers.school_year_id')
+                ->join('school_year', 'school_year.school_year_id', 'class_headers.school_year_id')
                 ->join('users', 'users.id', 'class_subjects.teacher_user_id')
                 ->join('users as userB', 'userB.id', 'class_headers.homeroom_teacher_user_id')
                 ->find($classSubjectId),
@@ -102,28 +102,28 @@ class MaterialController extends Controller
     public function viewChooseClassSubject() {
         if (auth()->user()->role->name === 'Student') {
             return view('material.index', [
-                'classSubjects' => ClassSubject::select('class_headers.id as classId','class_headers.name','school_years.year as schoolYear', 'school_years.semester as semester', 'users.name as homeroomTeacherName', 'class_headers.homeroom_teacher_user_id as homeroomTeacherId', 'users.user_code as homeRoomTeacherNuptk',
+                'classSubjects' => ClassSubject::select('class_headers.id as classId','class_headers.name','school_year.year as schoolYear', 'school_year.semester as semester', 'users.name as homeroomTeacherName', 'class_headers.homeroom_teacher_user_id as homeroomTeacherId', 'users.user_code as homeRoomTeacherNuptk',
                                     'class_subjects.id as subjectId', 'class_subjects.name as subjectName','user2.id as teacherId', 'user2.name as teacherName', 'user2.user_code as teacherNuptk')
                                     ->join('class_headers', 'class_headers.id', 'class_subjects.class_header_id')    
-                                    ->join('school_years','school_years.id','class_headers.school_year_id')
+                                    ->join('school_year','school_year.school_year_id','class_headers.school_year_id')
                                     ->join('class_details', 'class_details.class_header_id', 'class_headers.id')
                                     ->join('users', 'users.id', 'class_headers.homeroom_teacher_user_id')
                                     ->join('users as user2', 'user2.id', 'class_subjects.teacher_user_id')
-                                    ->join('roles','roles.id','users.role_id')
-                                    ->where('roles.name','Teacher')
+                                    ->join('role','role.role_id','users.role_id')
+                                    ->where('role.name','Teacher')
                                     ->where('class_details.student_user_id', auth()->user()->id)
                                     ->orderBy('class_headers.school_year_id', 'DESC')->get()
             ]);
         } else {
 
             return view('material.index',[
-                'classSubjects' => ClassSubject::select('class_subjects.id as id', 'class_subjects.name as name', 'class_headers.name as className', 'class_headers.id as classId','users.id as teacherId', 'users.name as teacherName', 'users.user_code as teacherNuptk', 'userB.name as homeroomTeacherName', 'userB.user_code as homeroomTeacherNuptk', 'school_years.year as schoolYear', 'school_years.semester as semester', 'school_years.id as schoolYearId')
+                'classSubjects' => ClassSubject::select('class_subjects.id as id', 'class_subjects.name as name', 'class_headers.name as className', 'class_headers.id as classId','users.id as teacherId', 'users.name as teacherName', 'users.user_code as teacherNuptk', 'userB.name as homeroomTeacherName', 'userB.user_code as homeroomTeacherNuptk', 'school_year.year as schoolYear', 'school_year.semester as semester', 'school_year.school_year_id as schoolYearId')
                                     ->join('users', 'users.id', 'class_subjects.teacher_user_id')
-                                    ->join('roles','roles.id','users.role_id')
+                                    ->join('role','role.role_id','users.role_id')
                                     ->join('class_headers', 'class_headers.id', 'class_subjects.class_header_id')
-                                    ->join('school_years','school_years.id','class_headers.school_year_id')
+                                    ->join('school_year','school_year.school_year_id','class_headers.school_year_id')
                                     ->join('users as userB', 'userB.id', 'class_headers.homeroom_teacher_user_id')
-                                    ->where('roles.name','Teacher')
+                                    ->where('role.name','Teacher')
                                     ->where('class_subjects.teacher_user_id', auth()->user()->id)
                                     ->orderBy('class_headers.school_year_id', 'DESC')->get()
             ]);
