1c8384d6eab7aa94cb3420e3ebaca7a27757557f
philipuswa77 <philipus.wijaya@gmail.com>
2023-05-23 21:24:10 +0700

    adjust forum controller

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -12,16 +12,16 @@ class ForumController extends Controller
     public function index($classSubjectId)
     {
         return view('forum.index', [
-            'forums' => Forum::select('forums.id', 'forums.teacher_user_id', 'forums.title', 'forums.class_subject_id')
+            'forums' => Forum::select('forums.id', 'forums.user_id', 'forums.title', 'forums.class_subject_id')
                 ->join('class_subjects', 'forums.class_subject_id', 'class_subjects.id')
                 ->where('class_subjects.id', $classSubjectId)->orderBy('id', 'desc')->get(),
             'classSubject' => ClassSubject::select('class_subjects.id as id', 'class_subjects.name as name',
-                'class_headers.name as className', 'school_years.year as schoolYear', 'school_years.semester as semester', 'users.name as teacherName',
-                'userB.name as homeRoomTeacherName', 'userB.user_code as homeRoomTeacherNuptk', 'users.user_code as teacherNuptk')
+                'class_headers.name as className', 'school_year.year as schoolYear', 'school_year.semester as semester', 'user.name as teacherName',
+                'userB.name as homeRoomTeacherName', 'userB.user_code as homeRoomTeacherNuptk', 'user.user_code as teacherNuptk')
                 ->join('class_headers', 'class_headers.id', 'class_subjects.class_header_id')
-                ->join('school_years', 'school_years.id', 'class_headers.school_year_id')
-                ->join('users', 'users.id', 'class_subjects.teacher_user_id')
-                ->join('users as userB', 'userB.id', 'class_headers.homeroom_teacher_user_id')
+                ->join('school_year', 'school_year.school_year_id', 'class_headers.school_year_id')
+                ->join('user', 'user.user_id', 'class_subjects.user_id')
+                ->join('user as userB', 'userB.user_id', 'class_headers.user_id')
                 ->find($classSubjectId),
         ]);
     }
@@ -97,29 +97,29 @@ class ForumController extends Controller
     public function viewChooseClassSubject() {
         if (auth()->user()->role->name === 'Student') {
             return view('forum.index', [
-                'classSubjects' => ClassSubject::select('class_headers.id as classId','class_headers.name','school_years.year as schoolYear', 'school_years.semester as semester', 'users.name as homeroomTeacherName', 'class_headers.homeroom_teacher_user_id as homeroomTeacherId', 'users.user_code as homeRoomTeacherNuptk',
+                'classSubjects' => ClassSubject::select('class_headers.id as classId','class_headers.name','school_year.year as schoolYear', 'school_year.semester as semester', 'user.name as homeroomTeacherName', 'class_headers.user_id as homeroomTeacherId', 'user.user_code as homeRoomTeacherNuptk',
                                     'class_subjects.id as subjectId', 'class_subjects.name as subjectName','user2.id as teacherId', 'user2.name as teacherName', 'user2.user_code as teacherNuptk')
                                     ->join('class_headers', 'class_headers.id', 'class_subjects.class_header_id')    
-                                    ->join('school_years','school_years.id','class_headers.school_year_id')
+                                    ->join('school_year','school_year.school_year_id','class_headers.school_year_id')
                                     ->join('class_details', 'class_details.class_header_id', 'class_headers.id')
-                                    ->join('users', 'users.id', 'class_headers.homeroom_teacher_user_id')
-                                    ->join('users as user2', 'user2.id', 'class_subjects.teacher_user_id')
-                                    ->join('roles','roles.id','users.role_id')
-                                    ->where('roles.name','Teacher')
-                                    ->where('class_details.student_user_id', auth()->user()->user_id)
+                                    ->join('user', 'user.user_id', 'class_headers.user_id')
+                                    ->join('user as user2', 'user2.id', 'class_subjects.user_id')
+                                    ->join('role','role.id','user.role_id')
+                                    ->where('role.name','Teacher')
+                                    ->where('class_details.user_id', auth()->user()->user_id)
                                     ->orderBy('class_headers.school_year_id', 'DESC')->get()
             ]);
         } else {
 
             return view('forum.index',[
-                'classSubjects' => ClassSubject::select('class_subjects.id as id', 'class_subjects.name as name', 'class_headers.name as className', 'class_headers.id as classId','users.id as teacherId', 'users.name as teacherName', 'users.user_code as teacherNuptk', 'userB.name as homeroomTeacherName', 'userB.user_code as homeroomTeacherNuptk', 'school_years.year as schoolYear', 'school_years.semester as semester', 'school_years.id as schoolYearId')
-                                    ->join('users', 'users.id', 'class_subjects.teacher_user_id')
-                                    ->join('roles','roles.id','users.role_id')
+                'classSubjects' => ClassSubject::select('class_subjects.id as id', 'class_subjects.name as name', 'class_headers.name as className', 'class_headers.id as classId','user.user_id as teacherId', 'user.name as teacherName', 'user.user_code as teacherNuptk', 'userB.name as homeroomTeacherName', 'userB.user_code as homeroomTeacherNuptk', 'school_year.year as schoolYear', 'school_year.semester as semester', 'school_year.school_year_id as schoolYearId')
+                                    ->join('user', 'user.user_id', 'class_subjects.user_id')
+                                    ->join('role','role.role_id','user.role_id')
                                     ->join('class_headers', 'class_headers.id', 'class_subjects.class_header_id')
-                                    ->join('school_years','school_years.id','class_headers.school_year_id')
-                                    ->join('users as userB', 'userB.id', 'class_headers.homeroom_teacher_user_id')
-                                    ->where('roles.name','Teacher')
-                                    ->where('class_subjects.teacher_user_id', auth()->user()->user_id)
+                                    ->join('school_year','school_year.school_year_id','class_headers.school_year_id')
+                                    ->join('user as userB', 'userB.user_id', 'class_headers.user_id')
+                                    ->where('role.name','Teacher')
+                                    ->where('class_subjects.user_id', auth()->user()->user_id)
                                     ->orderBy('class_headers.school_year_id', 'DESC')->get()
             ]);
         }
